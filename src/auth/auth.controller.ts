3a249b067d9c6b69fffb6232c5e73a81e0de1707
Lucas Cordeiro <lucas.cordeiro@mobilesaude.com.br>
2022-05-28 13:19:46 -0300

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { RegisterDto } from './dtos/register.dto';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcryptjs';
+
+@Controller()
+export class AuthController {
+  constructor(private userService: UserService) {}
+
+  @Post('admin/register')
+  async register(@Body() body: RegisterDto) {
+    const { password_confirm, ...data } = body;
+
+    if (body.password !== password_confirm) {
+      throw new BadRequestException('Passwords do not match !');
+    }
+
+    const hashed = await bcrypt.hash(body.password, 12);
+
+    return this.userService.save({
+      ...data,
+      password: hashed,
+      is_ambassador: false,
+    });
+  }
+}
