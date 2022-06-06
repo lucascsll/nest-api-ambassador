a9258025811fecc4785c68baede76476a7793fda
Lucas Cordeiro <lucas.cordeiro@mobilesaude.com.br>
2022-06-06 11:07:16 -0300

    Criar variaveis de ambiente para conexão com o banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'db',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'ambassador',
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.PORT,
+      username: process.env.USER_NAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       autoLoadEntities: true,
       synchronize: true,
-    }),
+    } as TypeOrmModuleOptions),
     UserModule,
     AuthModule,
   ],
