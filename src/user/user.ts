ac6b687f6ea9c742c2f91bad057a4b5426ff6536
Lucas Cordeiro <lucas.cordeiro@mobilesaude.com.br>
2022-06-07 23:12:21 -0300

    acrescentar password, created e update at

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,4 +1,9 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -14,6 +19,15 @@ export class User {
   @Column({ unique: true })
   email: string;
 
+  @Column()
+  password: string;
+
   @Column({ default: true })
   is_ambassador: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @CreateDateColumn({ name: 'update_at' })
+  updateAt: string;
 }
