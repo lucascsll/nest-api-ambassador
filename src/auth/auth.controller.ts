95248d6035acfb9e4d8be641d53b87914a60323f
Lucas Cordeiro <lucas.cordeiro@mobilesaude.com.br>
2022-06-08 00:16:24 -0300

    criar sesão de usuario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,32 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
-import { RegisterDto } from './dtos/register.dto';
-import { UserService } from '../user/user.service';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
 import * as bcrypt from 'bcryptjs';
 
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(private authServise: AuthService) {}
 
-  // @Post('admin/register')
-  // async register(@Body() body: RegisterDto) {
-  //   const { password_confirm, ...data } = body;
-  //
-  //   if (body.password !== password_confirm) {
-  //     throw new BadRequestException('Passwords do not match !');
-  //   }
-  //
-  //   const hashed = await bcrypt.hash(body.password, 12);
-  //
-  //   return this.userService.save({
-  //     ...data,
-  //     password: hashed,
-  //     is_ambassador: false,
-  //   });
-  // }
+  @Post('admin/session')
+  async creatSessios(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.authServise.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    return user;
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/user';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findOne(email) {
+    return this.userRepository.findOne(email);
+  }
+}
